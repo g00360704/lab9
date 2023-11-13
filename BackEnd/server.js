2d2b4188e0c9c86c1b7d75f8950f9bba6530d6c8
unknown <g00360704@atu.ie>
2023-11-13 15:50:45 +0000

    lab7

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -19,17 +19,57 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.goi4lkk.mongodb.net/?retryWrites=true&w=majority');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const bookSchema = new mongoose.Schema({
+  title:String,
+  cover:String,
+  author:String
+});
+
+const bookModel = mongoose.model('books',bookSchema);
 
 app.post('/api/book', (req,res)=>{
     console.log(req.body);
-    res.send("Book Created")
+
+       bookModel.create({
+        title:req.body.title,
+        cover:req.body.cover,
+        author:req.body.author
+  })
+    .then(
+      ()=>{res.send("Data Recieved!")}
+    )
+    .catch(
+      ()=>{res.send("Data NOT Recieved!")}
+    )
 })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/books', (req, res)=>{
+app.get('/api/books', async (req, res)=>{
+
+  let books = await bookModel.find({});
+  console.log(books)
+  res.json(books);
+})
+
+app.get('/api/book/:id', async (req,res)=>{
+  console.log(req.params.id);
+  let book = await bookModel.findById({_id:req.params.id})
+  res.send(book);
+})
     
 const data = [
     {
@@ -69,14 +109,7 @@ const data = [
     }
     ];
 
-    res.json({
-        myBooks:data,
-        "MyMessage":"Hello Data"
-    }) 
-
-})
-
-
+ 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
