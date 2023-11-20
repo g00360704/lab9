fc0c418e9bc10cdab6a80093e5033b9029a08a20
unknown <g00360704@atu.ie>
2023-11-20 15:43:47 +0000

    lab8 complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,117 +1,73 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const port = 4000;
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 app.use(cors());
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-const bodyParser = require("body-parser");
-
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// getting-started.js
-const mongoose = require('mongoose');
+async function main() {
+  await mongoose.connect('mongodb+srv://admin:admin@martinscluster.w5rtkz0.mongodb.net/DB14?retryWrites=true&w=majority');
+}
 
 main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb+srv://admin:admin@cluster0.goi4lkk.mongodb.net/?retryWrites=true&w=majority');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-
 const bookSchema = new mongoose.Schema({
-  title:String,
-  cover:String,
-  author:String
+  title: String,
+  cover: String,
+  author: String,
 });
-// Define a Mongoose model based on the book schema//
-const bookModel = mongoose.model('books',bookSchema);
 
-app.post('/api/book', (req,res)=>{
-    console.log(req.body);
- // Create a new book instance using the Book model//
-       bookModel.create({
-        title:req.body.title,
-        cover:req.body.cover,
-        author:req.body.author
-  })
-    .then(
-      ()=>{res.send("Data Recieved!")}
-    )
-    .catch(
-      ()=>{res.send("Data NOT Recieved!")}
-    )
-})
-// Route handling for the root endpoint //
+const bookModel = mongoose.model('my_books', bookSchema);
+
+app.post('/api/book', (req, res) => {
+  console.log(req.body);
+
+  bookModel
+    .create({
+      title: req.body.title,
+      cover: req.body.cover,
+      author: req.body.author,
+    })
+    .then(() => {
+      res.send('Book Created');
+    })
+    .catch(() => {
+      res.send('Book NOT Created');
+    });
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-// Route handling for the '/api/books' endpoint to retrieve all books //
-app.get('/api/books', async (req, res)=>{
-// Respond with a JSON representation of the retrieved books //
+  res.send('Hello World!');
+});
+
+app.get('/api/books', async (req, res) => {
   let books = await bookModel.find({});
-  console.log(books)
   res.json(books);
-})
+});
 
-app.get('/api/book/:id', async (req,res)=>{
-  console.log(req.params.id);
-  let book = await bookModel.findById({_id:req.params.id})
+app.get('/api/book/:identifier', async (req, res) => {
+  console.log(req.params.identifier);
+  let book = await bookModel.findById(req.params.identifier);
   res.send(book);
-})
-    
-const data = [
-    {
-    "title": "Learn Git in a Month of Lunches",
-    "isbn": "1617292419",
-    "pageCount": 0,
-    "thumbnailUrl":
-    "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg", "status": "MEAP",
-    "authors": ["Rick Umali"],
-    "categories": []
-    },
-    {
-    "title": "MongoDB in Action, Second Edition",
-    "isbn": "1617291609",
-    "pageCount": 0,
-    "thumbnailUrl":
-    "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-    "status": "MEAP",
-    "authors": [
-    "Kyle Banker",
-    "Peter Bakkum",
-    "Tim Hawkins",
-    "Shaun Verch",
-    "Douglas Garrett"
-    ],
-    "categories": []
-    },
-    {
-    "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-    "isbn": "1617292036",
-    "pageCount": 0,
-    "thumbnailUrl":
-    "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-    "status": "MEAP",
-    "authors": ["Simon Holmes"],
-    "categories": []
-    }
-    ];
+});
 
- 
-// Start the server and make it listen on the specified port //
-// Log a message to the console indicating that the server is running and listening on the specified port //
+app.put('/api/book/:identifier', async (req, res) => {
+  console.log('Edit: ' + req.params.identifier);
+  let book = await bookModel.findByIdAndUpdate(req.params.identifier, req.body, { new: true });
+  res.send(book);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,26 +1,23 @@
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 function BookItem(props) {
-
     return (
         <div>
             <Card>
                 <Card.Header>{props.myBook.title}</Card.Header>
                 <Card.Body>
                     <blockquote className="blockquote mb-0">
-                        <img src={props.myBook.cover}></img>
+                        <img src={props.myBook.cover} alt={props.myBook.title} />
                         <footer>
-                            {props.myBook.author[0]}
+                            {props.myBook.author}
                         </footer>
                     </blockquote>
                 </Card.Body>
+                <Link to={"/edit/" + props.myBook._id} className='btn btn-primary'>Edit</Link>
             </Card>
-            {/* <h3>{props.myBook.title}</h3>
-            <img src={props.myBook.thumbnailUrl}></img>
-            <p>{props.myBook.authors[0]}</p> */}
         </div>
     );
-
 }
 
 export default BookItem;
